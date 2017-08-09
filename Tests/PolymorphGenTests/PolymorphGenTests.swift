25490816d3d9c8c5d80b8d029685d74836873ae1
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-08-09 18:52:01 +0200

    add code builder

--- Tests/PolymorphGenTests/PolymorphGenTests.swift
+++ Tests/PolymorphGenTests/PolymorphGenTests.swift
@@ -16,8 +16,28 @@ class PolymorphGenTests: XCTestCase {
         }
     }
 
+    func testCodeBuilderWithTabs() {
+        let builder = CodeBuilder()
+        builder.add(line: "if test {").rightTab().add(line: "print(\"ok\")").leftTab().add(line: "}")
+        let res = builder.build()
+        XCTAssertEqual(res, "if test {\n\tprint(\"ok\")\n}\n")
+    }
+
+    func testCodeBuilderWithInnerCodeBuilder() {
+        let other = CodeBuilder()
+        other.add(line: "if test {").rightTab().add(line: "print(\"ok\")").leftTab().add(line: "}")
+
+        let builder = CodeBuilder()
+        builder.add(line: "if polymorph {").rightTab().add(code: other).leftTab().add(line: "}")
+
+        let res = builder.build()
+        XCTAssertEqual(res, "if polymorph {\n\tif test {\n\t\tprint(\"ok\")\n\t}\n}\n")
+    }
+
     static var allTests = [
         ("testEmptyGen", testEmptyGen),
+        ("testCodeBuilderWithTabs", testCodeBuilderWithTabs),
+        ("testCodeBuilderWithInnerCodeBuilder", testCodeBuilderWithInnerCodeBuilder)
     ]
 }
 
