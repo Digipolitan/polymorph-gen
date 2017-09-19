d3cc252d7bb04ac5190b33dbeebd8458e3fbcfca
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-09-19 17:39:13 +0200

    add class file builder

---
+++ Sources/PolymorphGen/ClassFileBuilder.swift
@@ -0,0 +1,26 @@
+//
+//  ClassFileBuilder.swift
+//  PolymorphGen
+//
+//  Created by Benoit BRIATTE on 19/09/2017.
+//
+
+import Foundation
+import PolymorphCore
+
+public protocol ClassFileBuilder {
+    func build(element: Class, options: PolymorphGen.Options) throws -> [File]
+}
+
+public class ClassFileBuilderArray: ClassFileBuilder {
+
+    private var children: [ClassFileBuilder] = []
+
+    public init(children: [ClassFileBuilder]) {
+        self.children = children
+    }
+
+    public func build(element: Class, options: PolymorphGen.Options) throws -> [File] {
+        return try self.children.map { try $0.build(element: element, options: options) }.reduce([], { return $0 + $1 })
+    }
+}
